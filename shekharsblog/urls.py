d5d7b41bf032637ae2c2ffe05989c7fe322adc83
Shekhar <patnaikshekhar@gmail.com>
2013-05-01 03:30:10 -0700

    Added development hook in urls to serve static files

--- shekharsblog/urls.py
+++ shekharsblog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +14,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
 )
+
+
+# Include static files in development mode
+urlpatterns += staticfiles_urlpatterns()
