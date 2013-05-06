4cd11e2e053dc0fa99e6150309d387c910efaea2
Shekhar <patnaikshekhar@gmail.com>
2013-05-05 19:43:30 -0700

    Added Mobile View for Index

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,46 @@ from django.utils import timezone
 from blog.models import Article, Comments, Project
 from blog.helperclasses import Archive
 
+
+"""
+This function detects if the browser is a mobile
+browser
+"""
+def isMobile(request):
+    mobile_ua_hints = [ 'SymbianOS', 'Opera Mini', 'iPhone' ]
+    mobile_uas = [
+        'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
+        'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
+        'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
+        'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
+        'newt','noki','oper','palm','pana','pant','phil','play','port','prox',
+        'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
+        'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
+        'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
+        'wapr','webc','winw','winw','xda','xda-'
+    ]
+
+    mobile_browser = False
+    ua = request.META['HTTP_USER_AGENT'].lower()[0:4]
+    if (ua in mobile_uas):
+        mobile_browser = True
+    else:
+        for hint in mobile_ua_hints:
+            if request.META['HTTP_USER_AGENT'].find(hint) > 0:
+                mobile_browser = True
+
+    return mobile_browser
+
 """
 The homepage view is rendered here
 """
 def index(request):
     latest_articles = Article.objects.all().order_by('-date')[:3]
     context = {'latest_articles': latest_articles}
-    return render(request, 'blog/index.html', context)
+    if isMobile(request):
+        return render(request, 'blog/index_m.html', context)
+    else:
+        return render(request, 'blog/index.html', context)
 
 
 """
